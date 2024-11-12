e1981124a78c0dd812ad2eff2e16f43d910fea62
Ahmad El-Mowafy <ahmadelmowafy4@gmail.com>
2024-11-12 18:13:22 +0000

    Completed feature 1

--- js/main.js
+++ js/main.js
@@ -1,28 +1,27 @@
-"use strict";
+'use strict';
+/* global data, writeEntry */
 const $form = document.querySelector('form');
-if (!$form)
-    throw new Error('$form not found');
+if (!$form) throw new Error('$form not found');
 const $img = document.querySelector('img');
 const $photoURLInput = document.querySelector('#photo-url');
 $photoURLInput?.addEventListener('change', () => {
-    $img?.setAttribute('src', $photoURLInput.value);
+  $img?.setAttribute('src', $photoURLInput.value);
 });
 $form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const $formElements = $form.elements;
-    const title = $formElements.namedItem('title').value;
-    const photoURL = $formElements.namedItem('photo').value;
-    const notes = $formElements.namedItem('notes').value;
-    const entry = {
-        title,
-        photoURL,
-        notes,
-        entryId: data.nextEntryId,
-    };
-    data.nextEntryId++;
-    data.entries.push(entry);
-    console.log(entry);
-    writeEntry();
-    $img?.setAttribute('src', 'images/placeholder-image-square.jpg');
-    $form.reset();
+  event.preventDefault();
+  const $formElements = $form.elements;
+  const title = $formElements.namedItem('title').value;
+  const photoURL = $formElements.namedItem('photo').value;
+  const notes = $formElements.namedItem('notes').value;
+  const entry = {
+    title,
+    photoURL,
+    notes,
+    entryId: data.nextEntryId,
+  };
+  data.nextEntryId++;
+  data.entries.push(entry);
+  writeEntry();
+  $img?.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
 });
